ac83fd6710808c947785a09af081c3534c72e494
MoturiHemasree <7hemasreemoturi.com>
2024-02-15 18:16:42 +0530

    upload file

---
+++ cat-n-light.js
@@ -0,0 +1,14 @@
+function off(){
+    document.getElementById("img").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-off-img.png";
+    document.getElementById("img1").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-eyes-img.png";
+    document.getElementById("s1").textContent="switched off";
+    document.getElementById("b1").style.backgroundColor="#cbd2d9"
+    document.getElementById("b2").style.backgroundColor="#22c55e"
+}
+function on(){
+    document.getElementById("img").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-on-img.png";
+    document.getElementById("img1").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-img.png";
+    document.getElementById("s1").textContent="switched on";
+    document.getElementById("b1").style.backgroundColor="#e12d39"
+    document.getElementById("b2").style.backgroundColor="#cbd2d9"
+}
